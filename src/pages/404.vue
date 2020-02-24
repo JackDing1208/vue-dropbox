77ce12fc42f889d15542a1281572a14eca6af67e
jack <361164217@qq.com>
2020-02-24 16:27:24 +0800

    404返回首页逻辑

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="container">
-        页面被狗吃了
+        <p>页面被狗吃了</p>
+        <el-button @click="backHome">返回首页</el-button>
     </div>
 </template>
 
 <script>
-  export default {}
+  export default {
+    methods: {
+      backHome() {
+        this.$router.replace("/dashboard")
+      },
+    },
+  }
 
 </script>
 
@@ -14,6 +21,5 @@
     .container {
         width: 100%;
         height: 100%;
-        display: flex;
     }
 </style>
