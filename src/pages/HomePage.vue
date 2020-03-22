9156cb45a863475b4341b56e1bd3af8a5c6d42fe
JackDing <361164217@qq.com>
2020-03-22 23:22:59 +0800

    主页菜单

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,22 @@
 <template>
     <div class="container">
-        <button @click="getUserGrid"> 发post请求</button>
+        <ul class="module-container"
+        >
+            <li v-for="(item ,index) in moduleArray"
+                :key="index.toString()"
+                @click="goToModule($event,item.route)"
+
+            >
+                <div :data-route="index.route">
+                    <div class="module-button" :style="{background:item.color}">
+                        <i class="el-icon-setting"></i>
+                    </div>
+                    <p>{{item.name}}</p>
+                </div>
+
+            </li>
+        </ul>
+
     </div>
 </template>
 
@@ -8,6 +24,24 @@
   import {http, url} from "../lib"
 
   export default {
+    data() {
+      return {
+        moduleArray: [
+          {name: "系统设置", icon: "haha", route: "system", color: "blue"},
+          {name: "部门管理", icon: "haha", route: "system", color: "yellow"},
+          {name: "用户管理", icon: "haha", route: "system", color: "blue"},
+          {name: "统计报表", icon: "haha", route: "system", color: "blue"},
+          {name: "日志审计", icon: "haha", route: "system", color: "pink"},
+          {name: "我的文档", icon: "haha", route: "system", color: "blue"},
+          {name: "部门文档", icon: "haha", route: "system", color: "blue"},
+          {name: "知识管理", icon: "haha", route: "system", color: "green"},
+          {name: "我的报表", icon: "haha", route: "system", color: "blue"},
+          {name: "帮助中心", icon: "haha", route: "system", color: "blue"},
+        ]
+      }
+    },
+
+
     methods: {
       getUserGrid() {
         http.post(url.getGridJson, {
@@ -19,8 +53,19 @@
             "record": "",
           },
           "Keyword": "",
-        }).then((res)=>{console.log(res)})
+        }).then((res) => {console.log(res)})
       },
+      goToModule(e,route) {
+        console.log(route)
+        // while (target.tagName.toLowerCase() !== "ul") {
+        //   if (target.tagName.toLowerCase() === "li") {
+        //     console.log(target)
+        //     console.log(target.getAttribute("data-route"))
+        //     break
+        //   }
+        //   target = target.parentNode
+        // }
+      }
     },
 
   }
@@ -33,6 +78,23 @@
         width: 100%;
         height: 100%;
         display: flex;
-        background: pink;
+    }
+
+    .module-container {
+        border: 1px solid red;
+        display: flex;
+        width: 100%;
+        justify-content: flex-start;
+        align-items: center;
+        flex-wrap: wrap;
+    }
+
+    .module-button {
+        height: 150px;
+        width: 150px;
+        margin: 0 50px;
+        border-radius: 20%;
+        border: 1px solid red;
+        flex-direction: column;
     }
 </style>
